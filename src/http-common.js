985ff49a8e8a9fb72f71f8acd49873427affdb87
lokreanand <lokreanand@gmail.com>
2023-05-08 14:25:58 +0530

    revert

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "https://employee-management-client.onrender.com/api",
+  baseURL: "http://localhost:3000/api",
   headers: {
     "Content-type": "application/json"
   }
