7742e55137278d0b67996e2268b4baf9c183fc2d
lokreanand <lokreanand@gmail.com>
2023-05-08 14:37:36 +0530

    updated http-common with deploy link

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://employee-management-client1.onrender.com/api",
   headers: {
     "Content-type": "application/json"
   }
